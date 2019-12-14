69bb7e076cb508c4ff02239c71cddbe20e9f382b
Bastien <bastien.ratat@gmail.com>
2019-12-15 00:29:27 +0800

    presentations edited on the about page

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
 from flask import Flask, render_template, url_for
 
 articles = {
-	"title": "First post",
-	"author": "Bastien Ratat",
-	"date": "2019, December 14",
-	"content": "waiting for being filled with interesting content"
+	"title": "post",
+	"author": "x",
+	"date": "xx/xx/xxxx",
+	"content": "x"
 }
 
-pages = ["Homepage", "About", "Products"]
+presentations = {
+	"Daniel":   """
+					Highly-motivated & ambitious SWUFE MIB senior year
+				    student with considerable 2 years track record &
+				    experience in business development & marketing incl. 
+				    successful creation & execution of creative digital 
+				    media project for the Government of Queensland, 
+				    effective coordination & implementation of e-commerce 
+				    marketing strategy - paired with strong organizational 
+				    & functional skills. Recognized appointed TA of SWUFE 
+				    prof. Xu Yongan & laureate of Chinese Government 
+				    Scholarship for SWUFE MIB Programme & HNU Chinese 
+				    Language and Culture Programme, SWUFE International 
+				    Students Performance Scholarship. Enterprising, 
+				    knowledgeable & ingenious Visionary, bring change and 
+				    innovation via creative performance & solutions.
+			    """,
+	"Bastien":  """  From Lyon, France, Bastien landed in Chendgu in 
+				    2014. Student, entrepreneur, civil servant, he's
+				    now heading to work in the tech world after he
+				    dove into programming.
+				"""
+}
+
+pages = ["Homepage", "About"]
+names = ["Daniel Biskupski", "Bastien Ratat"]
 
 app = Flask(__name__)
 
@@ -18,11 +43,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template("about.html", title='About', pages=pages)
-
-@app.route('/products')
-def products():
-    return render_template("products.html", title='Our products', pages=pages)
+    return render_template("about.html", pages=pages, names=names, presentations=presentations)
 
 if __name__ == '__main__':
 	app.run(debug=True)
